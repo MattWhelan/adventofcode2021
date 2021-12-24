3d65e0044fa6731aa55e23092b50524b05a0e974
Matt Whelan <matt@blacklogik.com>
2021-12-23 18:18:13 -0800

    Day 23 WIP

---
+++ day23/src/lib.rs
@@ -0,0 +1,370 @@
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, BTreeMap, HashMap, HashSet};
+use std::fmt::{Display, Formatter};
+use std::iter::once;
+use std::ops::Index;
+use std::rc::Rc;
+use std::str::FromStr;
+use itertools::Itertools;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+pub enum Color {
+    A, B, C, D
+}
+
+impl Color {
+    pub fn step_cost(&self) -> u32 {
+        match self {
+            Color::A => 1,
+            Color::B => 10,
+            Color::C => 100,
+            Color::D => 1000,
+        }
+    }
+}
+
+impl From<char> for Color {
+    fn from(ch: char) -> Self {
+        match ch {
+            'A' => Color::A,
+            'B' => Color::B,
+            'C' => Color::C,
+            'D' => Color::D,
+            ch => panic!("unsupported color {}", ch),
+        }
+    }
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let ch = match self {
+            Color::A => 'a',
+            Color::B => 'b',
+            Color::C => 'c',
+            Color::D => 'd',
+        };
+        write!(f, "{}", ch)
+    }
+}
+
+#[derive(Debug, Clone, Hash, Eq, PartialEq)]
+pub struct Pawn {
+    pub color: Color,
+    distance: u32,
+    start: (usize, usize),
+}
+
+impl Pawn {
+    pub fn new(color: Color, pos: (usize, usize)) -> Pawn {
+        Pawn {
+            color,
+            distance: 0,
+            start: pos,
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
+pub enum Tile {
+    WALL,
+    HOME(Color),
+    HALL
+}
+
+impl From<char> for Tile {
+    fn from(ch: char) -> Self {
+        match ch {
+            '#' => Tile::WALL,
+            '.' => Tile::HALL,
+            ch if ch.is_uppercase() => Tile::HOME(ch.into()),
+            ' ' => Tile::WALL,
+            _ => panic!("unexpected char {}", ch)
+        }
+    }
+}
+
+impl Display for Tile {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let ch = match self {
+            Tile::WALL => '#',
+            Tile::HOME(_) => '_',
+            Tile::HALL => '.'
+        };
+        write!(f, "{}", ch)
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct Maze {
+    grid: Vec<Vec<Tile>>,
+}
+
+impl FromStr for Maze {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let grid = s.lines()
+            .map(|l| l.chars()
+                .map(|ch| ch.into())
+                .collect()
+            )
+            .collect();
+        Ok(Maze {
+            grid
+        })
+    }
+}
+
+impl Maze {
+    pub fn pawns<'a>(&'a self) -> impl Iterator<Item = Pawn> + 'a {
+        self.grid.iter().enumerate()
+            .flat_map(|(y, row)| row.iter().enumerate()
+                .filter_map(move |(x, t)| if let Tile::HOME(c) = t {
+                    Some(Pawn::new(*c, (x, y)))
+                } else {
+                    None
+                })
+            )
+    }
+
+    pub fn neighbors<'a>(&'a self, (x, y): &(usize, usize)) -> impl Iterator<Item = (usize, usize)> + 'a {
+        let x_range = 0..self.grid[0].len() as isize;
+        let y_range = 0..self.grid.len() as isize;
+        let x = *x as isize;
+        let y = *y as isize;
+        (-1..=1)
+            .cartesian_product(-1..=1)
+            .filter(move |&(p, q)| {
+                ((p + q) as i32).abs() == 1
+                    && x_range.contains(&(x + p))
+                    && y_range.contains(&(y + q))
+            })
+            .map(move |(p, q)| ((x + p) as usize, (y + q) as usize))
+            .filter(|pos| self.is_open(pos))
+    }
+
+    fn is_open(&self, pos: &(usize, usize)) -> bool {
+        match self[pos] {
+            Tile::WALL => false,
+            Tile::HOME(_) => true,
+            Tile::HALL => true,
+        }
+    }
+
+    pub fn blocks_doorway(&self, pos: &(usize, usize)) -> bool {
+        match self[pos] {
+            Tile::WALL => false,
+            Tile::HOME(_) => false,
+            Tile::HALL => {
+                self.neighbors(pos).count() > 2
+            },
+        }
+    }
+
+    fn homes<'a>(&'a self) -> impl Iterator<Item=(usize, usize)> + 'a{
+        self.grid.iter().enumerate()
+            .flat_map(|(y, row)| row.iter().enumerate()
+                .map(move |(x, t)| ((x,y), t)))
+            .filter(|(_,t)| matches!(t, Tile::HOME(_)))
+            .map(|(p, _)| p)
+    }
+}
+
+impl Index<&(usize, usize)> for Maze {
+    type Output = Tile;
+
+    fn index(&self, index: &(usize, usize)) -> &Self::Output {
+        &self.grid[index.1][index.0]
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct VisitState {
+    dist: u32,
+    pos: (usize, usize),
+}
+
+impl Ord for VisitState {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.dist.cmp(&self.dist)
+            .then_with(|| self.pos.cmp(&other.pos))
+    }
+}
+
+// `PartialOrd` needs to be implemented as well.
+impl PartialOrd for VisitState {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct World {
+    maze: Rc<Maze>,
+    pawns: BTreeMap<(usize, usize), Pawn>,
+}
+
+impl World {
+    pub fn new(maze: Maze, pawns: Vec<Pawn>) -> World {
+        World {
+            maze: Rc::new(maze),
+            pawns: pawns.into_iter().map(|p| (p.start, p)).collect(),
+        }
+    }
+
+    fn is_vacant(&self, pos: &(usize, usize)) -> bool {
+        !self.pawns.contains_key(pos)
+    }
+
+    fn pawn_at(&self, pos: &(usize, usize)) -> Option<&Pawn> {
+        self.pawns.get(pos)
+    }
+
+    fn pawn_matches_room(&self, pos: &(usize, usize)) -> bool {
+        if let Some(pawn) = self.pawn_at(pos) {
+            if let Tile::HOME(home_color) = self.maze[pos] {
+                pawn.color == home_color
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    }
+
+    fn room<'a>(&'a self, pos: &(usize, usize)) -> impl Iterator<Item = (usize, usize)> + 'a {
+        assert!(matches!(self.tile(pos), Tile::HOME(_)));
+
+        self.maze.neighbors(pos)
+            .filter(|n| matches!(self.tile(n), Tile::HOME(_)))
+            .chain(once(*pos))
+    }
+
+    pub fn neighbor_count(&self, pos: &(usize, usize)) -> usize {
+        self.maze.neighbors(pos).count()
+    }
+
+    pub fn is_ready_home(&self, pos: &(usize, usize)) -> bool {
+        if matches!(self.tile(pos), Tile::HOME(_)) && self.is_vacant(pos) {
+            self.room(pos)
+                .all(|p| self.is_vacant(&p) || self.pawn_matches_room(&p))
+        } else {
+            false
+        }
+    }
+
+    pub fn is_pawn_in_hall(&self, pos: &(usize, usize)) -> bool {
+        if let Tile::HALL = self.maze[pos] {
+            self.pawns.contains_key(pos)
+        } else {
+            false
+        }
+    }
+
+    pub fn do_move(&mut self, from: &(usize, usize), to: &(usize, usize), distance: u32) {
+        assert!(self.is_vacant(to));
+        assert!(self.maze.is_open(to));
+        if let Some(mut p) = self.pawns.remove(from) {
+            if !self.pawns.contains_key(to) {
+                p.distance += distance;
+                self.pawns.insert(to.clone(), p);
+            } else {
+                panic!("move to occupied space at to {:?}", to)
+            }
+        } else {
+            panic!("no pawn at {:?}", from);
+        }
+    }
+
+    pub fn path_dist(&self, start: &(usize, usize)) -> HashMap<(usize, usize), u32> {
+        let mut distance = HashMap::new();
+        let mut visited = HashSet::new();
+        // let mut prev = HashMap::new();
+
+        distance.insert(*start, 0);
+
+        let mut heap = BinaryHeap::new();
+        heap.push(VisitState {
+            dist: 0,
+            pos: *start,
+        });
+
+        while let Some(VisitState { dist, pos: current }) = heap.pop() {
+            let current_cost = distance[&current];
+            if dist > current_cost || visited.contains(&current) {
+                continue;
+            }
+
+            if current_cost == u32::MAX {
+                // Disconnected; some obstructions exist
+                break;
+            }
+            for n in self.maze.neighbors(&current)
+                .filter(|n| !visited.contains(n) && self.is_vacant(n)) {
+                let d = distance.entry(n).or_insert(u32::MAX);
+                const COST: u32 = 1;
+                if *d > current_cost + COST {
+                    *d = current_cost + COST;
+                    // prev.insert(n, current);
+                    heap.push(VisitState { dist: *d, pos: n })
+                }
+            }
+            visited.insert(current);
+        }
+
+        distance
+    }
+
+    pub fn total_cost(&self) -> u32 {
+        self.pawns.values().map(|p| p.distance * p.color.step_cost()).sum()
+    }
+
+    pub fn is_settled(&self) -> bool {
+        assert_eq!(8, self.pawns.len());
+        self.pawns.iter()
+            .all(|(pos, _)| self.pawn_matches_room(pos))
+    }
+
+    pub fn pawns(&self) -> &BTreeMap<(usize, usize), Pawn> {
+        &self.pawns
+    }
+
+    pub fn find_ready_homes<'a>(&'a self) -> impl Iterator<Item=(usize, usize)> + 'a{
+        self.maze.homes()
+            .filter(|h| self.is_ready_home(h))
+    }
+
+    pub fn tile(&self, pos: &(usize, usize)) -> Tile {
+        self.maze[pos]
+    }
+
+    pub fn is_pawn_settled_at(&self, pos: &(usize, usize)) -> bool {
+        self.pawn_matches_room(pos) && self.room(pos)
+            .all(|place| self.is_vacant(&place) || self.pawn_matches_room(&place))
+    }
+
+    pub fn blocks_doorway(&self, pos: &(usize, usize)) -> bool {
+        self.maze.blocks_doorway(pos)
+    }
+}
+
+impl Display for World {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for y in 0..self.maze.grid.len() {
+            for x in 0..self.maze.grid[y].len() {
+                if let Some(pawn) = self.pawn_at(&(x, y)) {
+                    let mut s = pawn.color.to_string();
+                    if self.is_pawn_settled_at(&(x, y)) {
+                        s = s.to_uppercase();
+                    }
+                    write!(f, "{}", s).unwrap();
+                } else {
+                    let tile = self.tile(&(x, y));
+                    write!(f, "{}", tile).unwrap();
+                }
+            }
+            write!(f, "\n").unwrap();
+        }
+        Ok(())
+    }
+}
--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,22 +1,112 @@
+use std::collections::HashMap;
 use anyhow::Result;
-use std::str::FromStr;
-
-#[derive(Debug)]
-struct Record;
-
-impl FromStr for Record {
-    type Err = anyhow::Error;
-
-    fn from_str(_s: &str) -> Result<Self, Self::Err> {
-        Ok(Record)
-    }
-}
+use day23::*;
 
 fn main() -> Result<()> {
-    let input: Vec<Record> = INPUT.lines().map(|l| l.parse().unwrap()).collect();
+    let maze = TARGET.parse::<Maze>().unwrap();
+    let pawns: Vec<Pawn> = INPUT.parse::<Maze>().unwrap().pawns().collect();
 
-    dbg!(&input);
+    let world = World::new(maze, pawns);
+    println!("\nPart 1: {}", part1(world).expect("Failed to solve"));
+
     Ok(())
 }
 
-const INPUT: &str = r#""#;
+fn part1(world: World) -> Option<u32> {
+    if world.is_settled() {
+        Some(world.total_cost())
+    } else {
+        //println!("{}\n", &world);
+        //if there are move-home moves, do those
+        let ready_homes: HashMap<Color, (usize, usize)> = world.find_ready_homes()
+            .map(|h| if let Tile::HOME(color) = world.tile(&h) {
+                (color, h)
+            } else {
+                unreachable!()
+            })
+            .fold(HashMap::new(), |mut acc, (color, h)| {
+                // pick the furthest-in spot in the room
+                if let Some(pos) = acc.get(&color) {
+                    if world.neighbor_count(pos) > world.neighbor_count(&h) {
+                        acc.insert(color, h);
+                    }
+                } else {
+                    acc.insert(color, h);
+                }
+                acc
+            });
+        let move_homes: Vec<_> = if ready_homes.is_empty(){
+            Vec::new()
+        } else {
+            world.pawns().iter()
+                .filter_map(|(pawn_pos, pawn)| {
+                    if let Tile::HALL = world.tile(pawn_pos) {
+                        if let Some(ready_home_pos) = ready_homes.get(&pawn.color) {
+                            let dist = world.path_dist(pawn_pos);
+                            if let Some(distance) = dist.get(ready_home_pos) {
+                                Some((*pawn_pos, ready_home_pos, *distance))
+                            } else {
+                                None
+                            }
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                })
+                .collect()
+        };
+
+        if !move_homes.is_empty() {
+            move_homes.iter()
+                .filter_map(|(from, to, dist)| {
+                    let mut w = world.clone();
+                    w.do_move(from, to, *dist);
+                    part1(w)
+                })
+                .min()
+        } else {
+            //no move-homes, do a move-out
+
+            //find unsettled pawns
+            let move_outs: Vec<_> = world.pawns().iter()
+                .filter(|(pawn_pos, _)| {
+                    if let Tile::HOME(_) = world.tile(pawn_pos) {
+                        !world.is_pawn_settled_at(pawn_pos)
+                    } else {
+                        false
+                    }
+                })
+                .flat_map(|(pawn_pos, _)| {
+                    let dist = world.path_dist(pawn_pos);
+                    dist.into_iter()
+                        .filter(|(pos, _)| {
+                            matches!(world.tile(pos), Tile::HALL) && !world.blocks_doorway(pos)
+                        })
+                        .map(|(dest, cost)| (*pawn_pos, dest, cost))
+                })
+                .collect();
+            move_outs.iter()
+                .filter_map(|(from, to, dist)| {
+                    let mut w = world.clone();
+                    w.do_move(&from, &to, *dist);
+                    part1(w)
+                })
+                .min()
+        }
+    }
+}
+
+
+const INPUT: &str = r#"#############
+#...........#
+###C#A#B#D###
+  #B#A#D#C#
+  #########"#;
+
+const TARGET: &str = r#"#############
+#...........#
+###A#B#C#D###
+  #A#B#C#D#
+  #########"#;
